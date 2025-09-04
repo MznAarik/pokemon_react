7ec6554929cf4175726c5f76d9a9f670782e1fc8
MznAarik <119553332+MznAarik@users.noreply.github.com>
2025-09-04 16:24:14 +0545

    Update src/Pokemon.jsx
    Co-authored-by: codiumai-pr-agent-free[bot] <138128286+codiumai-pr-agent-free[bot]@users.noreply.github.com>

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -79,7 +79,7 @@ export const Pokemon = () => {
             </div>
 
             {/* Pokémon Cards */}
-            <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-[90rem]">
+            <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-[90rem]">
                 {searchData.map((poke) => (
                     <PokemonCards key={poke.id} data={poke} />
                 ))}
