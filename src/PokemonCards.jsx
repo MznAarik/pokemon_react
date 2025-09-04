6727f0a34d3856485f350e6a5f92114fbfc015a3
MznAarik <119553332+MznAarik@users.noreply.github.com>
2025-09-04 14:23:00 +0545

    Update src/PokemonCards.jsx
    Co-authored-by: codiumai-pr-agent-free[bot] <138128286+codiumai-pr-agent-free[bot]@users.noreply.github.com>

--- src/PokemonCards.jsx
+++ src/PokemonCards.jsx
@@ -103,7 +103,7 @@ export const PokemonCards = ({ data }) => {
               <p className={`me-2 mb-2 font-bold bg-gradient-to-r ${gradientClass} bg-clip-text text-transparent`}>
                 {data.stats[1].base_stat} <br />  Attack</p>
               <p className={`me-2 mb-2 font-bold bg-gradient-to-r ${gradientClass} bg-clip-text text-transparent`}>
-                {data?.abilities?.map((typeInfo)=>typeInfo.ability.name).slice(0,1).join(', ')} Ability
+                {data?.abilities?.[0]?.ability?.name || 'Unknown'} Ability
               </p>
 
             </div>
